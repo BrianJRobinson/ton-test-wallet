3590165a00e3d66bb85ebd0aa1892025c5efe3f0
BrianJRobinson <robinson.j.brian@gmail.com>
2024-09-23 22:02:56 +1000

    Update route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,17 +2,18 @@ import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
 export async function POST(req: NextRequest) {
+    let message: string = "";
     try {
         const userData = await req.json();
 
         if (!userData || !userData.id) {
             return NextResponse.json({ error: 'Invalid user data'}, { status: 400 });
         }
-
+        message = "Paste userdata check..."
         let user = await prisma.user.findUnique({
             where: { telegramId: userData.id }
         });
-
+        message = message + "about to call create..."
         if (!user) {
             user = await prisma.user.create({
                 data: {
@@ -23,8 +24,8 @@ export async function POST(req: NextRequest) {
                 }
             });
         }
-
-        return NextResponse.json({"user" : user, "extra" : "Attempted Call"});
+        message = message + "call complete..."
+        return NextResponse.json({"user" : user, "extra" : message});
     } catch (error) {
         console.error('Error processing user data:', error);
         return NextResponse.json({error: 'internal server error'}, { status: 500});
