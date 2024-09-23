95ab5a0ee458b49b2f3d1172eb4f2684925a3e60
BrianJRobinson <robinson.j.brian@gmail.com>
2024-09-23 21:04:15 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -146,6 +146,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center">
         <h1 className="text-4xl font-bold mb-8">TON Connect Demo</h1>
         <p> Current User: {tgUser?.id} : {tgUser?.username}</p>
+        <p>Errors: {error}</p>
         {tonWalletAddress ? (
           <div className="flex flex-col item-center">
             <p className="mb-4">Connected: {formatAddress(tonWalletAddress)}</p>
